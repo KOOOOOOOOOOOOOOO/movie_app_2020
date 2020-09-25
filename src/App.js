bdca3454f587ab6ab2f16c0a6c8d18c06dc2cd62
구승희 <pdw05460@gmail.com>
2020-09-25 15:25:24 +0900

    Protection with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Food({ name }) {
-  return <h1>I like {name} </h1>;
+function Food({ name,rating }) {
+  return(
+    <div>
+      <h2>I like {name}</h2>
+      <h4>{rating}</h4>
+    </div>
+  );
 }
 
 const FoodILike = [
   {
     id : 1,
-    name : "Coffee"
+    name : "Coffee",
+    rating : 3
   },
   {
     id : 2,
-    name : "Sandwich"
+    name : "Sandwich",
+    rating : 4
   }]
+
+
+Food.propTypes = {
+  name : PropTypes.string.isRequired,
+  rating : PropTypes.number.isRequired
+};
 
   function App() {
   return (
     <div>
       {FoodILike.map(dish => (
-        <Food key={dish.id} name={dish.name}/>
+        <Food key={dish.id} 
+              name={dish.name}
+              rating = {dish.rating}/>
       ))}
     </div>
   );
