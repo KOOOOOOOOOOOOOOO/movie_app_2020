093afa2ed10c92a0be5f92a8f5da11ddc09cb7f0
구승희 <pdw05460@gmail.com>
2020-09-28 19:17:47 +0900

    Class Components and State

--- src/App.js
+++ src/App.js
@@ -1,43 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Food({ name,rating }) {
-  return(
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}</h4>
-    </div>
-  );
-}
-
-const FoodILike = [
-  {
-    id : 1,
-    name : "Coffee",
-    rating : 3
-  },
-  {
-    id : 2,
-    name : "Sandwich",
-    rating : 4
-  }]
-
-
-Food.propTypes = {
-  name : PropTypes.string.isRequired,
-  rating : PropTypes.number.isRequired
-};
-
-  function App() {
-  return (
-    <div>
-      {FoodILike.map(dish => (
-        <Food key={dish.id} 
-              name={dish.name}
-              rating = {dish.rating}/>
-      ))}
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count :0
+  };
+  add = () => {
+    console.log("add");
+  }
+  minus = () => {
+    console.log("minus");
+  }
+  render(){
+    return(
+      <div>
+        <h1>The number is : {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    )
+  }
 }
 
 export default App;
